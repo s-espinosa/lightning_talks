26060343c0eeed8f6e3216317b04eef4c9957dcc
Sal Espinosa <sespinos@gmail.com>
2016-11-26 07:28:49 -0800

    Create test for project creation

---
+++ spec/features/user_can_create_a_project_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'When a user visits the new project path' do
+  it 'they can create a new project' do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit new_project_path
+    fill_in "project[group_members]", with: "Sharon Jones"
+    fill_in "project[name]", with: "Witty Name"
+    select "M2", from: "project[type]"
+    check "project[final_confirmation]"
+    click_on "Submit"
+
+    project = Project.last
+    expect(current_path).to eq(project_path(project))
+    expect(page).to have_content("Project successfully submitted!")
+    expect(page).to have_content("Witty Name")
+    expect(page).to have_content("Sharon Jones")
+  end
+end
