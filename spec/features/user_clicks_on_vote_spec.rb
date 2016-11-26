8a60571d706f4230815338e2dd285910e739a0d7
Sal Espinosa <sespinos@gmail.com>
2016-11-26 14:27:45 -0800

    Update descriptions to match test

--- spec/features/user_clicks_on_vote_spec.rb
+++ spec/features/user_clicks_on_vote_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'When a user clicks on Sign Up' do
-  it 'they are taken to create a new project' do
+describe 'When a user clicks on Vote' do
+  it 'they are taken to the project index' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
