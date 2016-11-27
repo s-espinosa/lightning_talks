57cc8ae2b0a420ffd899d4fd8c33f9227afd0e35
Sal Espinosa <sespinos@gmail.com>
2016-11-27 13:10:35 -0800

    Add ability for Admin to view list of projects

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/dashboard', to: 'dashboard#show'
+    resources :projects, only: [:index, :edit]
   end
 
   get '/login', to: 'sessions#new'
