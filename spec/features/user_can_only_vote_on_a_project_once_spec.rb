ee60b3a269a866558891172ae4087f6fff64629d
Mark Miranda <markmiranda@MacPro-102.local>
2016-12-16 11:28:26 -0700

    test written

--- spec/features/user_can_only_vote_on_a_project_once_spec.rb
+++ spec/features/user_can_only_vote_on_a_project_once_spec.rb
@@ -5,6 +5,29 @@ describe "When a user votes on a project" do
     demo_night = create(:demo_night_with_projects)
     project1, project2 =  demo_night.projects
 
-    
+    # As a user
+    user = create(:user)
+    # When I visit /demo_night/:id/projects
+    visit "/demo_night/#{demo_night.id}/projects"
+    # I am presented with a list of projects
+    within ('.unvoted') do
+      expect(page).to have_content(project1.name)
+      expect(page).to have_content(project2.name)
+    end
+
+    # When i click on a project
+    click_link("#{project1.name}")
+    # and I select a score for each of the categories
+    select "3", from: "vote[representation]"
+    select "3", from: "vote[challenge]"
+    select "3", from: "vote[wow]"
+    # and I click on submit
+    click_button("Submit")
+    # I am returned to /demo_night/:id/projects
+    expect(current_path).to eq("/demo_night/#{demo_night.id}/projects")
+    # And the project name of the project that I just voted on is no longer shown.
+    within ('.unvoted') do
+      expect(page).to_not have_content(project1.name)
+    end
   end
 end
