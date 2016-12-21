431516be42236dbb7cd1db59a85d40a47d7c5a77
Mark Miranda <markmiranda@MacPro-102.local>
2016-12-21 14:44:13 -0700

    start more styling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
       @project = Project.new
       @modules = ["BE Mod 2", "BE Mod 3", "BE Mod 4", "FE Mod 2", "FE Mod 3", "FE Mod 4", "Posse"]
     else
-      flash[:error] = "There is no active demo night, please ask Sal to make one"
+      flash[:error] = "There is no active demo night yet. BRB."
       redirect_to root_path
     end
   end
