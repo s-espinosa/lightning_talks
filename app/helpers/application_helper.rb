11ff503cf481cf13320b9e74b27e33610a1f3b53
Sal Espinosa <sespinos@gmail.com>
2016-12-28 06:37:50 -0700

    Update user UX for logging in to vote

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def accepting_submissions?
+    DemoNight.current != nil && DemoNight.current.accepting_submissions?
+  end
+
+  def current_demo_night?
+    DemoNight.current != nil
+  end
 end
