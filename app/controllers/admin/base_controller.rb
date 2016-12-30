428e07aad23c1caa486de611c48397d0a7c1fa94
Mark Miranda <markmiranda@MacPro-102.local>
2016-12-30 11:33:56 -0700

    ALL HAIL LORD SAL

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,6 @@ class Admin::BaseController < ApplicationController
   before_action :authorize_admin
 
   def authorize_admin
-    redirect_to errors_404_path unless current_user.admin?
+    raise ActionController::RoutingError.new('Not Found') unless current_user.admin?
   end
 end
