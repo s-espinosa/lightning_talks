2408553a8757a5e230de3039f12c3bb262b90fc6
Ryan Travitz <rtravitz@gmail.com>
2017-02-09 16:24:10 -0500

    Update user cannot see project notes to fail properly

--- spec/features/default/user_cannot_see_project_notes_spec.rb
+++ spec/features/default/user_cannot_see_project_notes_spec.rb
@@ -4,7 +4,7 @@ describe 'When a user visits the project index' do
   it 'they do not see notes for projects' do
     user    = create(:user)
     project = create(:project)
-    project.note = "make sure I win"
+    project.update(note: "make sure I win")
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit projects_path
