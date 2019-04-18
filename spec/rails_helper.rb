70a4bae33c10a94339c790e51a5d2265283bec11
Sal Espinosa <sespinos@gmail.com>
2019-04-17 20:11:57 -0600

    Update bundle for selenium and use travis docs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -53,13 +53,9 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {args: %(headless diable-gpu)}
-  )
+	options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
 
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
 Capybara.javascript_driver = :headless_chrome
